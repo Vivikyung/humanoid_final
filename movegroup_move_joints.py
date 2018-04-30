473813bd070a55b449edbf2c3b864a0043c51a4d
Vivian Shen <vivian@dyn-129-236-222-109.dyn.columbia.edu>
2018-04-29 21:54:27 -0400

    added a moveitpython interface file

--- movegroup_move_joints.py
+++ movegroup_move_joints.py
@@ -4,6 +4,7 @@ import sys
 import copy
 import rospy
 import moveit_commander
+import moveit_python
 import moveit_msgs.msg
 import geometry_msgs.msg
 
@@ -20,9 +21,11 @@ def move_joints():
     scene = moveit_commander.PlanningSceneInterface()
     
     # instantiate arm move group commanders
-    left = moveit_commander.MoveGroupCommander("left_arm")
-    right = moveit_commander.MoveGroupCommander("right_arm")
-    
+    left = moveit_python.move_group_interface.MoveGroupInterface("left_arm", "base")
+    right = moveit_python.move_group_interface.MoveGroupInterface("right_arm", "base")
+    left_joints = ['left_s0', 'left_s1', 'left_e0', 'left_e1', 'left_w0', 'left_w1', 'left_w2']
+    right_joints = ['right_s0', 'right_s1', 'right_e0', 'right_e1', 'right_w0', 'right_w1', 'right_w2']
+
     # publishes trajectory to visualize
     display_trajectory_publisher = rospy.Publisher(
                                         '/move_group/display_planned_path',
@@ -30,11 +33,11 @@ def move_joints():
                                         queue_size=20)
     
     # Gather basic information
-    print "============ Reference frame left: %s" % left.get_planning_frame()
-    print "============ Reference frame right: %s" % right.get_planning_frame()
-    
-    print "============ End effector left: %s" % left.get_end_effector_link()
-    print "============ End effector right: %s" % right.get_end_effector_link()
+    #print "============ Reference frame left: %s" % left.get_planning_frame()
+    #print "============ Reference frame right: %s" % right.get_planning_frame()
+    #
+    #print "============ End effector left: %s" % left.get_end_effector_link()
+    #print "============ End effector right: %s" % right.get_end_effector_link()
     
     print "============ Robot Groups:"
     print robot.get_group_names()
@@ -48,16 +51,17 @@ def move_joints():
     print "============"
 
     print "============ Printing left hand values"
-    print "%s" % left.get_end_effector_link()
+    #print "%s" % left.get_end_effector_link()
     
     # current set of left arm joint values
     #group_variable_values = left.get_current_joint_values()
     #print "============ Joint values left: %s" % group_variable_values
     #group_variable_values[0] = 1.0
-    d = {"left_w0":1.0}
-    left.set_joint_value_target(d)
+    #d = {"left_w0":1.0, "right_w0":1.0}
+    #left.set_joint_value_target(d)
     
-    plan = left.plan()
+    #plan = left.plan()
+    
     
     print "============ Waiting while RVIZ displays plan2..."
     rospy.sleep(5)
@@ -68,7 +72,7 @@ def move_joints():
     
     #left.execute(plan2)
 
-    left.clear_pose_targets()
+    #left.clear_pose_targets()
     
     moveit_commander.roscpp_shutdown()
 
--- test.py
+++ test.py
@@ -10,10 +10,10 @@ rospy.init_node("tmp")
 right_arm_move_group = moveit_python.move_group_interface.MoveGroupInterface("right_arm", "base")
 left_arm_move_group = moveit_python.move_group_interface.MoveGroupInterface("left_arm", "base")
 
-left_joint_names = ['left_s0', 'left_s1', 'left_e0', 'left_e1', 'left_w0', 'left_w1', 'left_w2', 'left_hand', 'left_endpoint']
+left_joint_names = ['left_s0', 'left_s1', 'left_e0', 'left_e1', 'left_w0', 'left_w1', 'left_w2']
 right_joint_names = ['right_s0', 'right_s1', 'right_e0', 'right_e1', 'right_w0', 'right_w1', 'right_w2', 'right_hand', 'right_endpoint']
 
-pose = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+pose = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
 left_arm_move_group.moveToJointPosition(left_joint_names, pose)
 left_arm_move_group.get_move_action().wait_for_result()
